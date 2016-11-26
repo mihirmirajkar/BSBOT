7b35c9b2626fa4143b5b21a9844ae4f185cf3111
mihirmirajkar <mihirmirajkar@gmail.com>
2016-11-26 00:39:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ var controller = Botkit.slackbot({
 
 // connect the bot to a stream of messages
 var myBot = controller.spawn({
-  token: 'xoxb-109957558358-IWjMybgzuAaWBm9qPDjtO64q',
+  token: 'xoxb-109957558358-p7NSxuArPrGSHHj8v9Sh47aj',
   incoming_webhook: {
 	url:  "https://hooks.slack.com/services/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"
   }
